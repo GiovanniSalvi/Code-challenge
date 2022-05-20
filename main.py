8bfca0f19eccab2ac72af059cc379acb5b89ecfd
GiovanniSalvi <salvigiovanni4@gmail.com>
2022-05-20 14:54:25 +0000

    commit

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ import pandas as pd
 
 df = pd.read_csv("/workspace/Code-challenge/cru-ts-2-10.1991-2000-cutdown.txt")
 df.T.to_csv('result.csv', header=False)
+
 print(df)
 #fileLines = fileLines.rstrip("\\n")
-character1 = ","
-fileString = fileLines.replace(character1, " ")
-character2 = "  "
-fileString = fileLines.replace(character2, " ")
-grids = fileString.split("Grid-ref=  ")
-for grid in grids:
-  splitGrids = grid.split(' ')
-  count = 0
-  for splitGrid in splitGrids:
-    if (count == 0):
-      xref = splitGrid
-      count += 1
-    if (count == 1):
-      yref = splitGrid
-      count += 1
-
-    print(xref)
-    print(yref)
-file.close()
+#character1 = ","
+#fileString = df.replace(character1, " ")
+#character2 = "  "
+#fileString = df.replace(character2, " ")
+#grids = fileString
+#for grid in grids:
+  #splitGrids = grid.split(' ')
+  #count = 0
+  #for splitGrid in splitGrids:
+    #if (count == 0):
+      #xref = splitGrid
+      #count += 1
+    #if (count == 1):
+      #yref = splitGrid
+      #count += 1
+#df.close()
 
 mydb = mysql.connector.connect(
   host="localhost",
