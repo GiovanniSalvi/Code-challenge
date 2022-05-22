91b320490234c42ccc141d904d4b1cb8f265b38f
GiovanniSalvi <salvigiovanni4@gmail.com>
2022-05-22 22:47:52 +0000

    commit

--- main.py
+++ main.py
@@ -2,28 +2,24 @@ import mysql.connector
 import pandas as pd
 import csv
 import psycopg2 
+import pymysql, sqlalchemy
 
 
-#df = pd.read_csv("/workspace/Code-challenge/cru-ts-2-10.1991-2000-cutdown.txt")
-#df.T.to_csv('result.csv', header=False, index=False)
-file_loc = "/workspace/Code-challenge/cru-ts-2-10.1991-2000-cutdown.txt"
-list = ['marker']
-read_df = pd.read_csv(file_loc, header=None, names=range(60)) #read in the csv file. names tells python how many columns to expect
-groups = read_df[0].isin(list).cumsum() #store the recurrence of 'marker'
-tables = {'process'+str(k): g.iloc[0:] for k,g in read_df.groupby(groups)} #make a dictionary of dataframes
+df = pd.read_csv("/workspace/Code-challenge/cru-ts-2-10.1991-2000-cutdown.txt")
+df.T.to_csv('result.csv', header=False, index=False)
 
-print(tables)
+print(df)
+#print(tables)
 
 mydb = mysql.connector.connect(
    host="127.0.0.1",
    port="3306",
    user="root",
    password="Giovanni",
-   db="mydatabase",
-   auth_plugin="mysql_native_password"
+   db="mydatabase"
 )
 mycursor = mydb.cursor()
-cursor.execute("CREATE DATABASE pysql")
+cursor.execute("CREATE DATABASE mydatabase")
 
 #fileLines = fileLines.rstrip("\\n")
 #character1 = ","
@@ -43,11 +39,11 @@ cursor.execute("CREATE DATABASE pysql")
 #df.close()
 
 
-#mycursor.execute("CREATE TABLE precipitations (xref INTEGER(255), yref INTEGER(255), date VARCHAR(255), val INTEGER(255))")
-#sqlTable = "INSERT INTO precipitations (xref, yref, date, val) VALUE (%s, %s, %s, %s)"
-#report = [('1','148','1/1/1991','3020'),
-       #('1', '148','2/1/1991','2820')]
+mycursor.execute("CREATE TABLE precipitations (xref INTEGER(255), yref INTEGER(255), date VARCHAR(255), val INTEGER(255))")
+sqlTable = "INSERT INTO precipitations (xref, yref, date, val) VALUE (%s, %s, %s, %s)"
+report = [('1','148','1/1/1991','3020'),
+       ('1', '148','2/1/1991','2820')]
 
 
-#mycursor.executemany(sqlTable, report)
-#mydb.commit()
+mycursor.executemany(sqlTable, report)
+mydb.commit()
